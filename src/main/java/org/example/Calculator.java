07c9606cbd3fb917f976b0d48cade1e5da8d22d5
Hoanglv <hoanglv1106@gmail.com>
2026-03-05 14:55:06 +0700

    feat : add caculator , hellowrd , unit test calculator

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,4 +1,21 @@
 package org.example;
-
+//Viết JUnit test kiểm tra hàm tính tổng 2 số.
 public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public long factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n must >= 0");
+        }
+
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,4 +1,29 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testSum() {
+        assertEquals(5, calculator.sum(2,3));
+    }
+
+    @Test
+    void testFactorial0() {
+        assertEquals(1, calculator.factorial(0));
+    }
+
+    @Test
+    void testFactorial1() {
+        assertEquals(1, calculator.factorial(1));
+    }
+
+    @Test
+    void testFactorial5() {
+        assertEquals(120, calculator.factorial(5));
+    }
 }
